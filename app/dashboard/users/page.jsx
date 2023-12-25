49cb0b487ad9b1a00ea2477d858d4658d7d6fe8c
Art San <artsan.mcc@gmail.com>
2023-12-25 14:08:27 +0700

    ok

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -1,35 +1,39 @@
+// import { deleteUser } from '@/app/lib/actions'
+import { fetchUsers } from '@/app/lib/data'
+import Pagination from '@/app/ui/dashboard/pagination/pagination'
+import Search from '@/app/ui/dashboard/search/search'
 import styles from '@/app/ui/dashboard/users/users.module.css'
-import Search from '@/app/ui/dashboard/search/Search'
-import Link from 'next/link'
 import Image from 'next/image'
-import Pagination from '@/app/ui/dashboard/pagination/Pagination'
-import { fetchUsers } from '../../lib/data.js'
+import Link from 'next/link'
 
-const UsersPage = async () => {
+const UsersPage = async ({ searchParams }) => {
+  // const q = searchParams?.q || "";
+  // const page = searchParams?.page || 1;
+  // const { count, users } = await fetchUsers(q, page);
   const users = await fetchUsers()
-  console.log('users', users)
+
   return (
-    <>
-      <div className={styles.container}>
-        <div className={styles.top}>
-          <Search placeholder="Search for a user..." />
-          <Link href="/dashboard/users/add">
-            <button className={styles.addButton}>Add New</button>
-          </Link>
-        </div>
-        <table className={styles.table}>
-          <thead>
-            <tr>
-              <td>Name</td>
-              <td>Email</td>
-              <td>Created At</td>
-              <td>Role</td>
-              <td>Status</td>
-              <td>Action</td>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder="Search for a user..." />
+        <Link href="/dashboard/users/add">
+          <button className={styles.addButton}>Add New</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Created At</td>
+            <td>Role</td>
+            <td>Status</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
               <td>
                 <div className={styles.user}>
                   <Image
@@ -39,22 +43,22 @@ const UsersPage = async () => {
                     height={40}
                     className={styles.userImage}
                   />
-                  John Doe
+                  {user.username}
                 </div>
               </td>
-              <td>{'user.email'}</td>
-              <td>{'13.01.22'}</td>
-              <td>{'Admin'}</td>
-              <td>{'Active'}</td>
+              <td>{user.email}</td>
+              <td>{user.createdAt?.toString().slice(4, 16)}</td>
+              <td>{user.isAdmin ? 'Admin' : 'Client'}</td>
+              <td>{user.isActive ? 'active' : 'passive'}</td>
               <td>
                 <div className={styles.buttons}>
-                  <Link href={`/dashboard/users/test`}>
+                  <Link href={`/dashboard/users/${user.id}`}>
                     <button className={`${styles.button} ${styles.view}`}>
                       View
                     </button>
                   </Link>
                   <form>
-                    <input type="hidden" name="id" />
+                    <input type="hidden" name="id" value={user.id} />
                     <button className={`${styles.button} ${styles.delete}`}>
                       Delete
                     </button>
@@ -62,76 +66,73 @@ const UsersPage = async () => {
                 </div>
               </td>
             </tr>
-          </tbody>
-        </table>
-        <Pagination />
-      </div>
-    </>
+          ))}
+        </tbody>
+      </table>
+      <Pagination />
+    </div>
   )
 }
 
 export default UsersPage
 
-// import { deleteUser } from "@/app/lib/actions";
-// import { fetchUsers } from "@/app/lib/data";
-// import Pagination from "@/app/ui/dashboard/pagination/pagination";
-// import Search from "@/app/ui/dashboard/search/search";
-// import styles from "@/app/ui/dashboard/users/users.module.css";
-// import Image from "next/image";
-// import Link from "next/link";
+// import styles from '@/app/ui/dashboard/users/users.module.css'
+// import Search from '@/app/ui/dashboard/search/Search'
+// import Link from 'next/link'
+// import Image from 'next/image'
+// import Pagination from '@/app/ui/dashboard/pagination/Pagination'
+// import { fetchUsers } from '../../lib/data.js'
 
-// const UsersPage = async ({ searchParams }) => {
-//   const q = searchParams?.q || "";
-//   const page = searchParams?.page || 1;
-//   const { count, users } = await fetchUsers(q, page);
-
+// const UsersPage = async () => {
+//   const users = await fetchUsers()
+//   console.log('users  1:58:59', users)
 //   return (
-//     <div className={styles.container}>
-//       <div className={styles.top}>
-//         <Search placeholder="Search for a user..." />
-//         <Link href="/dashboard/users/add">
-//           <button className={styles.addButton}>Add New</button>
-//         </Link>
-//       </div>
-//       <table className={styles.table}>
-//         <thead>
-//           <tr>
-//             <td>Name</td>
-//             <td>Email</td>
-//             <td>Created At</td>
-//             <td>Role</td>
-//             <td>Status</td>
-//             <td>Action</td>
-//           </tr>
-//         </thead>
-//         <tbody>
-//           {users.map((user) => (
-//             <tr key={user.id}>
+//     <>
+//       <div className={styles.container}>
+//         <div className={styles.top}>
+//           <Search placeholder="Search for a user..." />
+//           <Link href="/dashboard/users/add">
+//             <button className={styles.addButton}>Add New</button>
+//           </Link>
+//         </div>
+//         <table className={styles.table}>
+//           <thead>
+//             <tr>
+//               <td>Name</td>
+//               <td>Email</td>
+//               <td>Created At</td>
+//               <td>Role</td>
+//               <td>Status</td>
+//               <td>Action</td>
+//             </tr>
+//           </thead>
+//           <tbody>
+//             <tr>
 //               <td>
 //                 <div className={styles.user}>
 //                   <Image
-//                     src={user.img || "/noavatar.png"}
+//                     src={'/noAvatar.png'}
 //                     alt=""
 //                     width={40}
 //                     height={40}
 //                     className={styles.userImage}
 //                   />
-//                   {user.username}
+//                   John Doe
 //                 </div>
 //               </td>
-//               <td>{user.email}</td>
-//               <td>{user.createdAt?.toString().slice(4, 16)}</td>
-//               <td>{user.isAdmin ? "Admin" : "Client"}</td>
-//               <td>{user.isActive ? "active" : "passive"}</td>
+//               <td>{'user.email'}</td>
+//               <td>{'13.01.22'}</td>
+//               <td>{'Admin'}</td>
+//               <td>{'Active'}</td>
 //               <td>
 //                 <div className={styles.buttons}>
-//                   <Link href={`/dashboard/users/${user.id}`}>
+//                   <Link href={`/dashboard/users/test`}>
 //                     <button className={`${styles.button} ${styles.view}`}>
 //                       View
 //                     </button>
 //                   </Link>
-//                   <form action={deleteUser}>
-//                     <input type="hidden" name="id" value={(user.id)} />
+//                   <form>
+//                     <input type="hidden" name="id" />
 //                     <button className={`${styles.button} ${styles.delete}`}>
 //                       Delete
 //                     </button>
@@ -139,12 +140,12 @@ export default UsersPage
 //                 </div>
 //               </td>
 //             </tr>
-//           ))}
-//         </tbody>
-//       </table>
-//       <Pagination count={count} />
-//     </div>
-//   );
-// };
+//           </tbody>
+//         </table>
+//         <Pagination />
+//       </div>
+//     </>
+//   )
+// }
 
-// export default UsersPage;
+// export default UsersPage
--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,21 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <>
-      <div className="">
+      <div className=" flex flex-col gap-4">
         <h1>Home Page</h1>
         <ul>
           <li>Первый пункт</li>
           <li>Второй пункт</li>
           <li>Третий пункт</li>
         </ul>
+        <Link href="/dashboard/users">
+          <button className="bg-[#008080] py-[5px] px-2.5 text-text border-none cursor-pointer">
+            Dashboard
+          </button>
+        </Link>
       </div>
     </>
   )
