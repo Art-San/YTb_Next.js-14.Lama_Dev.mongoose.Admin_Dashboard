dd78d6df9bd42710cf8496cb75e91fe1eb5bc472
Art San <artsan.mcc@gmail.com>
2023-12-29 13:01:56 +0700

    ok

--- app/auth.config.js
+++ app/auth.config.js
@@ -1,20 +1,40 @@
+// export const authConfig = {
+//   providers: [],
+//   pages: {
+//     signIn: '/login'
+//   },
+//   callbacks: {
+//     authorized({ auth, request }) {
+//       const isLoggedIn = auth?.user
+//       // console.log('auth', auth)
+//       const isOnDashboard = request.nextUrl.pathname.startsWith('/dashboard')
+//       if (isOnDashboard) {
+//         if (isLoggedIn) return true
+//         return false
+//       } else if (isLoggedIn) {
+//         return Response.redirect(new URL('/dashboard', request.nextUrl))
+//       }
+//       return true
+//     }
+//   }
+// }
+
 export const authConfig = {
-  providers: [],
   pages: {
     signIn: '/login'
   },
   callbacks: {
-    authorized({ auth, request }) {
-      const isLoggedIn = auth?.user
-      // console.log('auth', auth)
-      const isOnDashboard = request.nextUrl.pathname.startsWith('/dashboard')
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user
+      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard')
       if (isOnDashboard) {
         if (isLoggedIn) return true
-        return false
+        return false // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
-        return Response.redirect(new URL('/dashboard', request.nextUrl))
+        return Response.redirect(new URL('/dashboard', nextUrl))
       }
       return true
     }
-  }
+  },
+  providers: [] // Add providers with an empty array for now
 }
